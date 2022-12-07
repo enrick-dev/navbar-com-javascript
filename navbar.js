3e7d833170b30a421c69090b3daf7bb5113d894c
Enrick Santos de Souza <enricksantos03@gmail.com>
2022-12-07 16:44:17 -0300

    'update:modifying-forEach-of-titles'

--- navbar.js
+++ navbar.js
@@ -5,28 +5,16 @@ let navTitles = [
   'Dev',
 ]
 
-navTitles.forEach((el) => {
-    let newUl = document.createElement('ul');
+let navEl = []
+
+navTitles.forEach((el,ind) => {
     let newLi = document.createElement('li');
     let newTitle = document.createTextNode(el);
     newLi.appendChild(newTitle);
-    newUl.appendChild(newLi);
 
-    var divAtual = document.getElementById("div1");
-    document.body.insertBefore(newUl, divAtual);
+    navEl[ind] = newLi;
+
+    var navAtual = document.getElementById("navbarUl");
+    navAtual.appendChild(newLi);
+    
 })
-
-// navTitles.forEach((el, ind) => {
-//   document.body.onload = addEl;
-
-//   function addEl() {
-//     let newLi = []
-//     newLi[ind] = document.createElement('li');
-//     let newTitle = []
-//     newTitle[ind] = document.createTextNode(el);
-//     newLi[ind].appendChild(newTitle[ind]);
-
-//     var divAtual = document.getElementById("div1");
-//     document.body.insertBefore(newLi[ind], divAtual[ind]);
-//   }
-// })
